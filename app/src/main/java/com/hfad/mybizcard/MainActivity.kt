283538c370b25dc77dcbbe8c6baa43863c03696a
peterken1 <ratemop@gmail.com>
2022-08-05 21:35:32 +0200

    update

--- app/src/main/java/com/hfad/mybizcard/MainActivity.kt
+++ app/src/main/java/com/hfad/mybizcard/MainActivity.kt
@@ -3,13 +3,18 @@ package com.hfad.mybizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hfad.mybizcard.ui.theme.MybizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +27,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CreateBizCard()
+
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateBizCard(){
+    Surface( modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+      Card(modifier = Modifier.width(200.dp)
+          .height(390.dp)
+          .padding(12.dp),
+          shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+          backgroundColor = Color.White,
+           elevation = 4.dp) {
+
+      }  
+    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MybizCardTheme {
-        Greeting("Android")
+        CreateBizCard()
+
     }
 }
